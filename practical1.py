cae80fb92f6c259fc2b2b883051f831d8116d6d0
Ankit Patil <123143098+16SULPHUR@users.noreply.github.com>
2023-02-14 18:55:24 +0530

    Create practical1.py
    practical 1 of python

---
+++ practical1.py
@@ -0,0 +1,6 @@
+student_name=raw_input("Enter Your Name: ")
+enroll_num=raw_input("Enter Your Enrollment No.: ")
+student_branch=raw_input("Enter Your Branch Name: ")
+student_collage=raw_input("Enter Your College Name: ")
+
+print("welcome dear {0} having enrollment number {1} to {2} in {3}".format(student_name,enroll_num,student_branch,student_collage))
